13883b52052eb5d3f1aff5eb046fdce1be5875f7
mulwa <mulwatech@gmail.com>
2018-07-24 04:59:12 +0300

    finalized creating account  module

--- app/src/main/java/com/example/gen/supermum/creatAccount.java
+++ app/src/main/java/com/example/gen/supermum/creatAccount.java
@@ -6,6 +6,8 @@ import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +17,16 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.gen.supermum.Pojo.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
@@ -26,6 +38,10 @@ public class creatAccount extends AppCompatActivity {
     private DatePickerDialog.OnDateSetListener mDataListener;
     private Calendar cal;
     private ProgressDialog mProgressDialog;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,10 @@ public class creatAccount extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         cal = Calendar.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("Users");
 
         if(getSupportActionBar() != null){
             getSupportActionBar().setTitle("Account Creation");
@@ -55,8 +75,22 @@ public class creatAccount extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                if(validateValues()){
-                   showToast("Ready to Save details");
-                   startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                   showDialog();
+                   mAuth.createUserWithEmailAndPassword(mEmail.getText().toString().trim(),mPassword.getText().toString().trim())
+                           .addOnCompleteListener(creatAccount.this, new OnCompleteListener<AuthResult>() {
+                               @Override
+                               public void onComplete(@NonNull Task<AuthResult> task) {
+                                   if(task.isSuccessful()){
+                                       currentUser = mAuth.getCurrentUser();
+                                       saveUser(currentUser.getUid());
+
+                                   }else {
+                                       hideDialog();
+                                       showToast("Authentication failed."+task.getException());
+                                   }
+                               }
+                           });
+
                }
 
             }
@@ -73,6 +107,14 @@ public class creatAccount extends AppCompatActivity {
                 lauchDateicker();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuth.getCurrentUser()  !=  null){
+            currentUser = mAuth.getCurrentUser();
+        }
     }
 
     public void initiazeView(){
@@ -92,6 +134,25 @@ public class creatAccount extends AppCompatActivity {
         username = mUsername.getText().toString().trim();
         conceptionDate = mConceptionDate.getText().toString().trim();
         password = mPassword.getText().toString().trim();
+    }
+    private void saveUser(String uid){
+        getValues();
+        User user = new User(username,email,mobile,conceptionDate,uid);
+        userRef.child(uid).setValue(user, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                hideDialog();
+                if(databaseError == null){
+                    showToast("Account Created Successfully");
+                    startActivity(new Intent(creatAccount.this,Login.class));
+
+                }else{
+                    showToast("Failed to create Account Please Try later");
+                }
+
+            }
+        });
+
     }
 
     public boolean validateValues(){
@@ -155,6 +216,5 @@ public class creatAccount extends AppCompatActivity {
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
         }
-
     }
 }
